424fa32b8f1f59edf0b47e77b95f0f87c41bc04b
Levy <mlev3@allstate.com>
2019-10-24 16:49:27 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
+
+    this.state = {
+      ideas: [
+        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+      ]
+    }
   }
 
   render() {
     return (
-      <h1>IdeaBox</h1>
+      <main className='App'>
+        <h1>IdeaBox</h1>
+        <p>Hi!</p>
+      </main>
     )
   }
 }
